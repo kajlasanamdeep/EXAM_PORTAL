a271e71ad2f5e2469e62a08caf4efc2080d272b5
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-06-22 16:31:42 +0530

    FIRST commit by sanam

--- handlers/user.handlers.js
+++ handlers/user.handlers.js
@@ -7,16 +7,15 @@ const statusCodes = statusCodeList.STATUS_CODE;
 const messages = messageList.MESSAGES;
 const { default: mongoose } = require('mongoose');
 
+module.exports.registerHandler = async function (req, res) {
+    try {
+        const { error, value } = validator.validateRegister(req);
+        if (error) return universalFunction.validationError(res, error.message);
+        const existingUser = await users.findOne({
+            email: value.email
+        })
+        if (existingUser) return universalFunction.sendResponse(res, statusCodes.UNPROCESSABLE_ENTITY, messages.EMAIL_ALREDAY_EXIT);
+    } catch (error) {
 
-module.exports.registerHandler = async function (req,res) {
-try {
-    const {error,value} = validator.validateRegister(req);
-    if(error) return universalFunction.validationError(res,error.message);
-    const existingUser = await users.findOne({
-        email:value.email
-    })
-    if(existingUser) return universalFunction.sendResponse(res,statusCodes.UNPROCESSABLE_ENTITY, messages.EMAIL_ALREDAY_EXIT);
-} catch (error) {
-    
-}    
+    }
 }
