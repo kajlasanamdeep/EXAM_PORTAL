5c17ad08c71c768b2d5494ea94e622389a99e2bb
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-06-22 19:00:05 +0530

    sanam testing

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,10 +1,5 @@
 const Handler = require('../handlers/user.handlers');
 const universalFunction = require('../lib/universal-function');
-const messageList = require("../messages/messages");
-const statusCodeList = require("../statusCodes/status_codes");
-const statusCodes = statusCodeList.STATUS_CODE;
-const messages = messageList.MESSAGES;
-const userHandler = require('../handlers/user.handlers')
 const { default: mongoose } = require('mongoose');
 
 module.exports.register = async function(req, res) {
@@ -16,15 +11,13 @@ try {
 }
 }
 
-module.exports.loginHandler = async function(res,res){
-    try{
+module.exports.login = async function(req,res){
+try{
     const {email,password} = req.body;
-    const data = await userHandler.loginHandler(email,password);
+    const data = await Handler.loginHandler(email,password);
      universalFunction.sendResponse(res,data.status,data.message,data);
     }
     catch(err){
         universalFunction.validationError(res,err)
     }
-    
-
 }
--- handlers/user.handlers.js
+++ handlers/user.handlers.js
@@ -1,5 +1,5 @@
 const universalFunction = require('../lib/universal-function');
-const users= require('../models/users');
+const users = require('../models/users');
 const messageList = require("../messages/messages");
 const validator = require('../validations/user.validations');
 const statusCodeList = require("../statusCodes/status_codes");
@@ -16,18 +16,18 @@ module.exports.registerHandler = async function (req, res) {
         const { error, value } = validator.validateRegister(req);
         if (error) throw error;
 
-        let existingUser = await users.findOne({email:value.email});
+        let existingUser = await users.findOne({ email: value.email });
 
         if (existingUser) return {
-            status:statusCodes.UNPROCESSABLE_ENTITY,
-            message:messages.EMAIL_ALREDAY_TAKEN
+            status: statusCodes.UNPROCESSABLE_ENTITY,
+            message: messages.EMAIL_ALREDAY_TAKEN
         };
 
-        existingUser = await users.findOne({mobileNumber:value.mobileNumber});
+        existingUser = await users.findOne({ mobileNumber: value.mobileNumber });
 
         if (existingUser) return {
-            status:statusCodes.UNPROCESSABLE_ENTITY,
-            message:messages.MOBILE_NUMBER_ALREADY_TAKEN
+            status: statusCodes.UNPROCESSABLE_ENTITY,
+            message: messages.MOBILE_NUMBER_ALREADY_TAKEN
         };
 
         value.password = await universalFunction.hashPasswordUsingBcrypt(value.password);
@@ -36,8 +36,8 @@ module.exports.registerHandler = async function (req, res) {
         let accessToken = await universalFunction.jwtSign(user);
 
         return {
-            status:statusCodes.SUCCESS,
-            message:messages.USER_REGISTER_SUCCESSFULLY,
+            status: statusCodes.SUCCESS,
+            message: messages.USER_REGISTER_SUCCESSFULLY,
             accessToken: accessToken,
             user: user
         };
@@ -49,33 +49,37 @@ module.exports.registerHandler = async function (req, res) {
     }
 }
 
-module.exports.loginHandler = async function(email,password) {
-    let user = await users.findOne({
-        email:email
-    })
-    if(!user)  return {
-        status: statusCodes.BAD_REQUEST,
-        message:messages.USER_NOT_FOUND
-    } 
+module.exports.loginHandler = async function (email, password) {
+    try {
 
-    let password = await comparePasswordUsingBcrypt(password)
-    try{
-    if(!password){
+        let user = await users.findOne({
+            email: email
+        });
+
+        if (!user) return {
+            status: statusCodes.BAD_REQUEST,
+            message: messages.USER_NOT_FOUND
+        };
+
+        let checkPassword = await comparePasswordUsingBcrypt(password);
+
+        if (!checkPassword) {
+            return {
+                status: statusCodes.BAD_REQUEST,
+                message: messages.INVALID_PASSWORD
+            }
+        };
+
+        let accessToken = await universalFunction.jwtSign(user);
+
         return {
-            status:statusCodes.BAD_REQUEST,
-            message:messages.INVALID_PASSWORD
+            status: statusCodes.SUCCESS,
+            message: messages.USER_LOGIN_SUCCESSFULLY,
+            accessToken: accessToken,
+            user: user
         }
     }
-    let accessToken = await universalFunction.jwtSign(user);
-    return {
-        status:statusCodes.SUCCESS,
-        message:messages.USER_LOGIN_SUCCESSFULLY,
-        accessToken:accessToken,
-        user:user
+    catch (error) {
+        throw error;
     }
-}
-catch(err){
-    throw err;
-}
-
 }
