bc4cc7b0b01e7b899fe9ed67e0a97afcd19b016c
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-06-22 18:43:17 +0530

    sanam register examiner

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,12 +1,15 @@
+const Handler = require('../handlers/user.handlers');
 const universalFunction = require('../lib/universal-function');
-const users = require('../models/users');
 const messageList = require("../messages/messages");
-const validator = require('../validations/user.validations');
 const statusCodeList = require("../statusCodes/status_codes");
 const statusCodes = statusCodeList.STATUS_CODE;
 const messages = messageList.MESSAGES;
-const { default: mongoose } = require('mongoose');
 
 module.exports.register = async function(req, res) {
-    
+try {
+    const data = await Handler.registerHandler(req,res);
+    return universalFunction.sendResponse(res,data.status,data.message,data);
+} catch (error) {
+    universalFunction.validationError(res, error.details);
+}
 }
--- handlers/user.handlers.js
+++ handlers/user.handlers.js
@@ -6,16 +6,44 @@ const statusCodeList = require("../statusCodes/status_codes");
 const statusCodes = statusCodeList.STATUS_CODE;
 const messages = messageList.MESSAGES;
 const { default: mongoose } = require('mongoose');
+const APP_CONSTANTS = require('../constant/APP_CONSTANTS');
 
 module.exports.registerHandler = async function (req, res) {
     try {
+        req.body.userType = APP_CONSTANTS.ACCOUNT_TYPE.EXAMINER;
+
         const { error, value } = validator.validateRegister(req);
-        if (error) return universalFunction.validationError(res, error.message);
-        const existingUser = await users.findOne({
-            email: value.email
-        })
-        if (existingUser) return universalFunction.sendResponse(res, statusCodes.UNPROCESSABLE_ENTITY, messages.EMAIL_ALREDAY_EXIT);
+        if (error) throw error;
+
+        let existingUser = await users.findOne({email:value.email});
+
+        if (existingUser) return {
+            status:statusCodes.UNPROCESSABLE_ENTITY,
+            message:messages.EMAIL_ALREDAY_TAKEN
+        };
+
+        existingUser = await users.findOne({mobileNumber:value.mobileNumber});
+
+        if (existingUser) return {
+            status:statusCodes.UNPROCESSABLE_ENTITY,
+            message:messages.MOBILE_NUMBER_ALREADY_TAKEN
+        };
+
+        value.password = await universalFunction.hashPasswordUsingBcrypt(value.password);
+        value.status = APP_CONSTANTS.ACCOUNT_STATUS.PENDING;
+        let user = await users.create(value);
+        let accessToken = await universalFunction.jwtSign(user);
+
+        return {
+            status:statusCodes.SUCCESS,
+            message:messages.USER_REGISTER_SUCCESSFULLY,
+            accessToken: accessToken,
+            user: user
+        };
+
     } catch (error) {
+
+        throw error;
 
     }
 }
