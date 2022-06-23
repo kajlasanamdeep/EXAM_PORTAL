6adb8dd0ca797d42df61598c0ab5b9f246cb1e3c
hritiksantoshi <hritiksantoshi99@gmail.com>
2022-06-23 16:11:29 +0530

    changes in admin controller

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
-const adminHandler = require('../handlers/admin.handler')
+const adminHandler = require('../handlers/admin.handler');
+const universalFunction = require('../lib/universal-function');
 
 module.exports.approveOrDeclineExaminer =  async function(req,res) {
+    try{
     let payload = req.body;
-
     let responseData = await adminHandler.approveOrDeclineExaminer(payload);
+    }
+    catch(err){
+        throw err;
+    }
 
+}
+
+module.exports.pendingExaminers = async function(req,res) {
+    try{
+    let examiners = await adminHandler.pendingExaminers();
+    return universalFunction.sendResponse(res,examiners.status,examiners.msg,examiners);
+    }
+    catch(err){
+    throw err;
+    }
 }
--- handlers/admin.handler.js
+++ handlers/admin.handler.js
@@ -1,15 +1,60 @@
-const mongoose = require('mongoose');
+const universalFunction = require('../lib/universal-function');
+const mongoose = require("mongoose");
+const APP_CONSTANTS = require("../constant/APP_CONSTANTS");
+const users = require("../models/users");
+const statusCodeList = require("../statusCodes/statusCodes");
+const statusCodes = statusCodeList.STATUS_CODE;
+const messageList = require("../messages/messages");
+const messages = messageList.MESSAGES;
+const generatorPassword = require('generate-password');
 
-module.exports.getExaminerDetails = async function(payload){
-    try{
-         
-    }
-    catch (err) {
-        throw err;
-    }
+module.exports.getExaminerDetails = async function (payload) {
+  try {
+  } catch (err) {
+    throw err;
+  }
+};
 
-}
+module.exports.approveOrDeclineExaminer = async function (payload) {
+  let query = {_id:mongoose.Types.ObjectId(payload.examinerId)};
 
-module.exports.approveOrDeclineExaminer = async function(payload){
-   let 
-}
+  let newPassword = generatorPassword.generate({
+    length:14,
+    numbers:true,
+    uppercase:true
+   });
+   let options = { new: true };
+
+   let hashedPassword = universalFunction.hashPasswordUsingBcrypt(newPassword);
+
+   let toUpdate = {
+    status:payload.status,
+    password: hashedPassword
+   }
+   
+   let updateExaminer = await users.findOneAndUpdate(query,toUpdate,options)
+};
+
+module.exports.pendingExaminers = async function () {
+  try {
+    let query = {
+      $and: [
+        { userType: APP_CONSTANTS.ACCOUNT_TYPE.EXAMINER },
+        { status: APP_CONSTANTS.ACCOUNT_STATUS.PENDING },
+      ],
+    };
+    let pendingExaminers = await users.find(query);
+    let count = await users.countDocuments(query);
+
+    return {
+      status: statusCodes.SUCCESS,
+      msg: messages.SUCCESS,
+      data: {
+        count: count,
+        pendingExaminers: pendingExaminers,
+      },
+    };
+  } catch (err) {
+    throw err;
+  }
+};
