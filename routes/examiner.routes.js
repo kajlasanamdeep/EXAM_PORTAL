f888d5d7a1360e6a2e50664b326c0688b5c81309
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-06-29 18:37:01 +0530

    readded add student

--- routes/examiner.routes.js
+++ routes/examiner.routes.js
@@ -3,7 +3,8 @@ const controller = require('../controllers').examiner;
 const userAuthorization = require('../auth/user.authorization');
 
 router.post('/course',userAuthorization,controller.createCourse);
-router.get('/dashboard',userAuthorization,controller.getDashboard)
+router.get('/dashboard',userAuthorization,controller.getDashboard);
+router.post('/student',userAuthorization,controller.addStudent);
 router.get('/students/:courseID',userAuthorization,controller.getStudent);
 
 module.exports = router;
